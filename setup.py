51463ec8bc2228853be77ad9eb9d3462e0a3bc6e
Martin Eriksen <martin.b.eriksen@gmail.com>
2013-02-24 06:00:39 +0100

    Fixed setup.py to install the configuration.

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ from setuptools import setup, find_packages
 name = 'Martin B. Eriksen'
 email = 'martin.b.eriksen@gmail.com'
 
+data_files = [
+    ('bcnz/config', glob.glob('bcnz/config/*.yaml'))]
+
 setup(
     name = 'bcnz',
     version = '1',
@@ -20,6 +23,7 @@ setup(
         'Tables'],
     author = name,
     author_email = email,
+    data_files = data_files,
     license = 'Read LICENSE.txt',
     maintainer = name,
     maintainer_email = email,
