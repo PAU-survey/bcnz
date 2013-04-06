a3130f33348f9927f8d3d0b8bdc656d1b031853f
Martin Eriksen <martin.b.eriksen@gmail.com>
2013-04-06 03:06:54 +0200

    Fix since the DES sv had large errors which made the program crash.

--- bcnz/observ/toflux.py
+++ bcnz/observ/toflux.py
@@ -3,6 +3,7 @@
 
 import pdb
 import numpy as np
+import scipy
 
 def toflux(conf, zdata, data):
     """Convert input magnitude to fluxes."""
@@ -12,26 +13,35 @@ def toflux(conf, zdata, data):
     mag = data['mag']
     emag = data['emag']
 
-    not_seen = (emag == conf['undet'])
+    print('XXXX')
+    A1 = mag == conf['unobs']
+    A2 = emag == conf['unobs']
+    A3 = conf['max_magerr'] < emag
+    not_obs = np.logical_or(A1, np.logical_or(A2, A3))
 
-    A1 = emag == conf['unobs']
-    A2 = conf['max_magerr'] < emag
-    not_obs = np.logical_or(A1, A2)
+    B1 = (mag == conf['undet'])
+    B2 = (emag == conf['undet'])
+    not_seen = np.logical_or(B1, B2)
 #    not_obs = np.logical_or(emag == conf['unobs'], conf['max_magerr'] < emag)
 
     seen = np.logical_not(np.logical_or(not_seen, not_obs))
     assert (1*seen + 1*not_seen + 1*not_obs == 1).all()
 
-    print(np.min(emag), np.max(emag))
+#    print(np.min(emag), np.max(emag))
 #    pdb.set_trace()
-    emag = np.clip(emag, conf['min_magerr'], np.inf)
+
+    # Clipping on mag value since some values can be unreasonable high..
+    emag = np.clip(emag, conf['min_magerr'], 150.)
 
     assert np.max(np.abs(seen*mag)) < 30
     assert np.max(seen*emag) < 5.
 
     # Convert to fluxes
+#    X = scipy.seterr(over='ignore')
     f_obs = seen*(10**(-0.4*mag))
     ef_obs = seen*(10**(0.4*emag)-1)*f_obs
+    scipy.seterr(**X)
+    pdb.set_trace()
 
     # One exception...
 #    ef_obs += not_seen*(10**(-0.4*np.abs(ef_obs)))
