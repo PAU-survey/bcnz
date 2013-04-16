684e6047d588b070c99c2d8568a1ba75641b835d
Martin Eriksen <martin.b.eriksen@gmail.com>
2013-04-16 02:23:21 +0200

    Changes to implement the bt task interface.

--- bcnz/lib/libconf.py
+++ bcnz/lib/libconf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: UTF8
 
+import copy
 import os
 import pdb
 import sys
@@ -15,6 +16,40 @@ class conf(dict):
         self.update(bcnz.config.conf['standard'].copy()) # HACK
         self.update(myconf)
         self._test_zrange()
+
+    def config_sample(self):
+        def flatten(d, pre=[]):
+            """Flatten a dictionary tree with parameters."""
+
+            if isinstance(d, dict):
+                res = {}
+                for key,val in d.iteritems():
+                    res.update(flatten(val,pre+[key]))
+
+                return res
+            else:
+                return {'.'.join(pre): d}
+
+        return flatten(self)
+
+    def schema(self):
+        """JSON schema Tallada-style."""
+
+        global tojson
+        tojson = {str: 'string', int: 'integer', float: 'float',
+                  list: 'list', bool: 'bolean', unicode: 'str'}
+
+        flatten_conf = self.config_sample()
+
+        defconf = copy.deepcopy(self)
+        injson = ["'" + key + "': { 'type': '"+tojson[type(val)]+"'}" for \
+                  key,val in flatten_conf.iteritems()]
+
+        # Produce a string with the schema.
+        txt = """{'title': 'BCNZ Schema', 'type': 'object', 'properties': {"""
+        txt += ',\n'.join(injson)+'}'
+
+        return txt
 
     def _test_zrange(self):
 
