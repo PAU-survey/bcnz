77b0a3ad3272ec46499d09390c351d9f99c0b41b
Martin Eriksen <martin.b.eriksen@gmail.com>
2013-09-25 22:59:22 +0200

    Changes the test for magnitudes.

--- bcnz/observ/toflux.py
+++ bcnz/observ/toflux.py
@@ -13,33 +13,49 @@ def toflux(conf, zdata, data):
     mag = data['mag']
     emag = data['emag']
 
-    A1 = mag == conf['unobs']
-    A2 = emag == conf['unobs']
-    not_obs = np.logical_or(A1, A2)
+    # The problem here is all possible ways people are specifying how
+    # to not use certain magnitudes. If *any* of the requirements fail,
+    # then do not use the observations. 
 
-    # Defines not_det as a subset of not_obs
-    B1 = (mag == conf['undet'])
-    B2 = (emag == conf['undet'])
-    B3 = conf['max_magerr'] < emag
-    not_det = np.logical_or(B1, np.logical_or(B2, B3))
-    not_det = np.logical_and(not_det, np.logical_not(not_obs))
+    unreal_mag = 50.
+    unreal_err = 5.
 
-    not_use = not_det 
-    touse = np.logical_not(not_use)
+    np_or = np.logical_or 
+    not_use = np.zeros(shape=mag.shape, dtype=np.bool)
+    not_use = np_or(not_use, mag == conf['unobs'])
+    not_use = np_or(not_use, emag == conf['unobs'])
+    not_use = np_or(not_use, mag == conf['undet'])
+    not_use = np_or(not_use, emag == conf['undet'])
+    not_use = np_or(not_use, conf['max_magerr'] < emag)
+    not_use = np_or(not_use, unreal_mag < mag)
 
-    # Clipping on mag value since some values can be unreasonable high..
+    to_use = np.logical_not(not_use)
+
+    # Note: One will get zero from filtering. I want to avoid people entering
+    # absolute magnitudes.
+    if np.min(to_use*mag) < 0:
+        ids = data['id'][1 <= (to_use*mag <= 0).sum(axis=1)]
+        raise ValueError('Negative magnitudes for ids: {0}'.format(ids))
+
+    if unreal_mag < np.max(to_use*mag):
+        ids = data['id'][1 <= (to_use*mag < unreal_mag).sum(axis=1)]
+        raise ValueError('Internal error for ids: {0}'.format(ids))
+
+    if unreal_err < np.max(to_use*emag):
+        ids = data['id'][1 <= (unreal_err < to_use*emag).sum(axis=1)]
+        raise ValueError('Unrealistic (>{0}) errors for ids: {1}'.format(unreal_err, ids))
+
+    # Too high magnitude errors can cause problems in the chi2
+    # expression.
     emag = np.clip(emag, conf['min_magerr'], 150.)
 
-    assert np.max(np.abs(touse*mag)) < 30
-    assert np.max(touse*emag) < 5.
+    assert np.max(np.abs(to_use*mag)) < 30
+    assert np.max(to_use*emag) < 5.
 
-    # Convert to fluxes
+    # Convert from maginitudes to fluxes
 #    X = scipy.seterr(over='ignore')
-    f_obs = touse*(10**(-0.4*mag))
-    ef_obs = touse*(10**(0.4*emag)-1)*f_obs
-
-    # One exception...
-#    ef_obs += not_seen*(10**(-0.4*np.abs(ef_obs)))
+    f_obs = to_use*(10**(-0.4*mag))
+    ef_obs = to_use*(10**(0.4*emag)-1)*f_obs
 
     assert (0. <= f_obs).all()
     ef_obs = np.where(not_use, 1e108, ef_obs)
