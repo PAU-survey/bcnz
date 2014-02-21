8235e9ad1d36b34a21d59c66c27ca28b40bc28c6
Martin B. Eriksen <martin.b.eriksen@gmail.com>
2014-02-21 14:59:59 +0100

    Created code for storing AB files in HDF5.

--- bcnz/model/project.py
+++ bcnz/model/project.py
@@ -12,6 +12,8 @@ from scipy.interpolate import splrep, splev
 from scipy.integrate import simps, trapz, quad
 from scipy.ndimage.interpolation import zoom
 
+import tables
+
 np.seterr('raise')
 
 class model_mag(object):
@@ -29,7 +31,16 @@ class model_mag(object):
         self.sed_spls = zdata['sed_spls']
         self.set_ninterp(zdata['filters'])
 
-    def proj_filter(self, d, z_ab, filter_name):
+
+    def _ab_path(self):
+        """Directory where the AB files are stored."""
+
+        ab_dir = os.path.join(os.environ['HOME'], self.conf['cache_dir'], \
+                              self.conf['ab_dir'], 'the_only_ab.hdf5')
+
+        return ab_dir
+
+    def _proj_filter(self, z_ab, seds, filter_name):
         """Project into filter."""
 
         xrlim = self.zdata['rlim'][filter_name]
@@ -45,7 +56,7 @@ class model_mag(object):
         a_ab = 1./(1.+z_ab)
 
         res = {}
-        for sed in self.zdata['seds']:
+        for sed in seds:
             sed_spl = self.sed_spls[sed]
 
             ker = np.outer(a_ab, xm)
@@ -55,11 +66,68 @@ class model_mag(object):
             bla = np.sum(xpart * hmm, axis=1)
             AB = np.vstack([z_ab, bla]).T
 
-            file_path = os.path.join(d, '%s.%s.AB' % (sed, filter_name))
-            np.savetxt(file_path, AB)
+#            file_path = os.path.join(d, '%s.%s.AB' % (sed, filter_name))
+#            np.savetxt(file_path, AB)
+            res[sed] = AB
 
         return res
 
+
+    def _all_proj(self):
+        """Project into the combination of all filters and SEDS."""
+
+        filters = self.zdata['filters']
+        zmax_ab = self.conf['zmax_ab']
+        dz_ab = self.conf['dz_ab']
+
+        z_ab = np.arange(0., zmax_ab, dz_ab)
+
+        # This will cause problems if switching the set of templates.
+        # A proper solution should first detect all available templates
+        # on the system.
+        seds = self.zdata['seds'] 
+
+        ab = {}
+        for filter_name in filters:
+            ab[filter_name] = self._proj_filter(z_ab, seds, filter_name)
+
+        return ab
+
+    def ensure_cached(self):
+        """Create cache of the AB files unless it exists."""
+
+        ab_path = self._ab_path()
+        if os.path.exists(ab_path):
+            return
+
+        ab = self._all_proj()
+
+        descr = {'z': tables.FloatCol(pos=0),
+                 'ab': tables.FloatCol(pos=1)}
+
+        # Start writing the results to a file. The format is (in theory)
+        # documented elsewhere.
+        fb = tables.openFile(ab_path, 'w')
+        for filter_name, abD in ab.iteritems():
+            # A change of name is needed sine HDF5 does not support
+            # storing nodes with numerical names.
+            try:
+                fname_store = 'NB{0}'.format(filter_name)
+            except ValueError:
+                fname_store = filter_name
+
+            fb.createGroup('/', fname_store)
+            for sed_name, AB in abD.iteritems():
+                grname = '/{0}'.format(fname_store)
+                ab_table = fb.createTable(grname, sed_name, descr)
+                ab_table.append(AB)
+                ab_table.close()
+
+
+        fb.close()
+
+
+    # Functions used when loading the results.
     def f_mod(self, z):
         """Model frequencies."""
 
@@ -71,9 +139,6 @@ class model_mag(object):
         filters = self.zdata['filters']
         z = self.zdata['z']
 
-        # HACK. Does not handle new filters added..
-        if not os.listdir(dir_ab):
-            self.all_proj()
 
         # This method is not the fastest, but works slightly faster
         # than the linear interpolation in BPZ!
@@ -91,19 +156,7 @@ class model_mag(object):
 
         return f_mod
 
-    def all_proj(self):
-        filters = self.zdata['filters']
-        zmax_ab = self.conf['zmax_ab']
-        dz_ab = self.conf['dz_ab']
 
-        z_ab = np.arange(0., zmax_ab, dz_ab)
-        ab_dir = os.path.join(os.environ['HOME'], self.conf['cache_dir'], self.conf['ab_dir'])
-
-        if not os.path.exists(ab_dir):
-            os.makedirs(ab_dir)
-
-        for filter_name in filters:
-            self.proj_filter(ab_dir, z_ab, filter_name)
 
 
     def interp(self, f_mod):
@@ -118,4 +171,4 @@ class model_mag(object):
         return f_new
 
     def __call__(self):
-        self.all_proj()
+        self.ensure_cached()
