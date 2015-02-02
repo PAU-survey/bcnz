05cad01c5d84a05f6a025e4661b28bd019d086b0
Martin B. Eriksen <martin.b.eriksen@gmail.com>
2015-02-02 12:44:35 +0100

    Removing code not in use.

--- bcnz/io/filebase.py
+++ bcnz/io/filebase.py
@@ -8,37 +8,13 @@ import shutil
 import bcnz.lib
 
 class filebase(object):
+# This code was introduced as an experiment, allowing the photo-z code to store 
+# many different catalogs by providing unique names. This functionality is by
+# now assumed to be provided by an external code.
+# 
+
     def setup(self):
-        obj_name = bcnz.lib.obj_hash.hash_structure(self.conf)
-
-        if self.conf['cache_dir']:
-            self.cache_tdir = os.path.join(self.conf['cache_dir'],\
-                                          'pzcat')
-        else:
-            root_dir = self.conf['root']
-            self.cache_dir = os.path.join(root_dir, 'cache')
-
-        cache_dir = self.conf['cache_dir'] if 'cache_dir' in self.conf \
-                    else os.path.join(self.conf['root'], 'cache')
-
-        self._obj_path_peaks = os.path.join(cache_dir, 'pzcat', obj_name)
-        self._obj_path_pdfs = os.path.join(cache_dir, 'pzpdf', obj_name)
-
-    def run_file(self):
-        assert False, 'Is this actually in use????'
-
-        if not os.path.isdir(self.cache_dir):
-            os.mkdir(self.cache_dir)
-
-        if self.conf['use_cache']:
-            if os.path.isfile(self.obj_path):
-                self.relink()
-                return
-
-            self.estimate_photoz(self.obj_path)
-            self.relink()
-        else:
-            self.estimate_photoz(self.out_name)
+        pass
 
     @property
     def out_path(self):
