094843fc8262532864a450de1a528c573a8f6b46
Martin B. Eriksen <eriksen@pic.es>
2017-09-07 16:10:37 +0200

    More tests of the fitting.

--- bcnz/tasks/bcnz_fit.py
+++ bcnz/tasks/bcnz_fit.py
@@ -23,15 +23,15 @@ descr = {
   'zmax': 'Maximum redshift',
   'dz': 'Grid width in redshift',
   'chi2_algo': 'The chi2 algorithm',
-  'use_lines': 'If including emission lines'
+  'use_lines': 'If including emission lines',
+  'use_ext': 'If including extinction'
 }
 
-
 class bcnz_fit:
-    """Fitting the fluxes to a galaxy template."""
+#    """Fitting the fluxes to a galaxy template."""
+    """Testing adding extinction."""
 
-    # New version of the bcnz core code.
-    version = 1.056
+    version = 1.057
     config = {
       'filters': [],
       'seds': [],
@@ -39,83 +39,85 @@ class bcnz_fit:
       'zmax': 2.0,
       'dz': 0.01,
       'odds_lim': 0.01,
-      'Niter': 300,
+      'Niter': 200,
       'line_weight': 2.,
       'chi2_algo': 'min',
-      'use_lines': True
+      'use_lines': True,
+      'use_ext': False
     }
 
     def check_conf(self):
         assert self.config['filters'], 'Need to set filters'
         assert self.config['seds'], 'Need to set: seds'
 
-    def _find_models(self, ab, seds):
-        """Find a list of models."""
+    def rebin_redshift(self, f_mod, zgrid):
+        """Rebin the model to the grid used in the calculations."""
 
-        models = []
-        for key in ab.columns:
-            if key == 'z':
-                continue
+        t1 = time.time()
+        z = f_mod.z.values
 
-            fname, sed, ext = key
-            if not sed in seds:
-                continue
+        # This order is chosen for hopefully better memory access 
+        # times.
+        nz = len(zgrid)
+        nband = len(f_mod.band)
+        nmodel = len(f_mod.model)
+        A = np.ones((nz, nmodel, nband))
 
-            models.append((sed, ext))
+        print('Starting to regrid')
+        for i,model in enumerate(f_mod.model.values):
+            for j,band in enumerate(f_mod.band.values):
+                y = f_mod.sel(band=band, model=model).values
+                spl = splrep(z, y)
+                A[:,i,j] = splev(zgrid, spl)
 
-        models = list(set(models))
+        print('Time regridding', time.time() - t1)
 
-        return models
+        coords = {'z': zgrid, 'band': f_mod.band, 'model': f_mod.model}
+        fmod_new = xr.DataArray(A, coords=coords, dims=('z', 'model', 'band'))
+
+        return fmod_new
 
     def _model_array(self, ab, zgrid, fL, seds):
         """Construct the model array."""
 
-        models = self._find_models(ab, seds)
+        ab = ab.set_index(['band','sed', 'z', 'EBV'])
+        f_mod = ab.to_xarray().flux
+        f_mod = f_mod.stack(model=['sed', 'EBV'])
 
-        f_mod = np.zeros((len(fL), len(models), len(zgrid)))
-        for i,fname in enumerate(fL):
-            for j,(sed,ext) in enumerate(models):
-                key = (fname, sed, ext)
-                spl = splrep(ab['z'], ab[key])
-                f_mod[i,j,:] = splev(zgrid, spl)
-
-
-        # Ok... this is not exactly pretty..
-        model = list(map('_'.join, models))
-        coords = {'f': fL, 'model': model, 'z': zgrid}
-        f_mod = xr.DataArray(f_mod, coords=coords, dims=('f','model','z'))
+        f_mod = f_mod.sel(band=self.config['filters'])
+        f_mod = self.rebin_redshift(f_mod, zgrid)
 
         return f_mod
 
+    def select_lines(self, ab_lines):
 
-    def _model_normal(self, ab, zgrid, fL):
-        """Array with the model."""
+        # This method could be extended if wanting different templates
+        # for the different emission lines.
+        ab_lines = ab_lines[ab_lines.sed == 'lines']
+        seds = ['lines']
 
-        seds = self.config['seds']
-        f_mod = self._model_array(ab, zgrid, fL, seds)
+        return ab_lines, seds
 
-        return f_mod
+    def model(self, ab_cont, ab_lines):
 
-    def _model_lines(self, ab, zgrid, fL):
-        return self._model_array(ab, zgrid, fL, ['lines'])
-
-#        ipdb.set_trace()
-
-    def model(self, ab, ab_el):
+        if not self.config['use_ext']:
+            ab_cont = ab_cont[ab_cont.EBV == 0.]
+            ab_lines = ab_lines[ab_lines.EBV == 0.]
 
         C = self.config
+        fL = C['filters']
+
         zgrid = np.arange(C['zmin'], C['zmax']+C['dz'], C['dz'])
-        fL = self.config['filters']
+        fmod_cont = self._model_array(ab_cont, zgrid, fL, C['seds'])
 
-        f_mod1 = self._model_normal(ab, zgrid, fL)
         if self.config['use_lines']:
-            f_mod2 = self._model_lines(ab_el, zgrid, fL)
-            f_mod = xr.concat([f_mod1, f_mod2], dim='model')
-        else:
-            f_mod = f_mod1
+            ab_lines, seds_lines = self.select_lines(ab_lines)
+            fmod_lines = self._model_array(ab_lines, zgrid, fL, seds_lines)
+            fmod = xr.concat([fmod_cont, fmod_lines], dim='model')
+        else: 
+            fmod = fmod_cont
 
-        return f_mod
-
+        return fmod
 
     def get_arrays(self, data_df):
         """Read in the arrays and present them as xarrays."""
@@ -123,7 +125,7 @@ class bcnz_fit:
         # Seperating this book keeping also makes it simpler to write
         # up different algorithms.
         filters = self.config['filters']
-        dims = ('gal', 'f')
+        dims = ('gal', 'band')
         flux = xr.DataArray(data_df['flux'][filters], dims=dims)
         flux_err = xr.DataArray(data_df['flux_err'][filters], dims=dims)
 
@@ -159,36 +161,31 @@ class bcnz_fit:
     def chi2_min(self, f_mod, data_df, zs): #, mabs_df):
         """Minimize the chi2 expression."""
 
-#        data_df = data_df[100:150]
         flux, flux_err, var_inv = self.get_arrays(data_df)
 
-
         t1 = time.time()
-        A = np.einsum('fsz,ftz,gf->gzst', f_mod, f_mod, var_inv)
+        A = np.einsum('gf,zsf,ztf->gzst', var_inv, f_mod, f_mod)
+
         print('time A',  time.time() - t1)
 
         t1 = time.time()
-        b = np.einsum('gf,fsz,gf->gzs', flux, f_mod, var_inv)
+        b = np.einsum('gf,gf,zsf->gzs', var_inv, flux, f_mod)
         print('time b',  time.time() - t1)
 
         Ap = np.where(A > 0, A, 0)
         An = np.where(A < 0, -A, 0)
 
-
         v = 100*np.ones_like(b)
 
         gal_id = np.array(data_df.index)
-        coords = {'gal': gal_id, 'f': f_mod.f, 'z': f_mod.z}
+        coords = {'gal': gal_id, 'band': f_mod.band, 'z': f_mod.z}
         coords_norm = {'gal': gal_id, 'z': f_mod.z, 'model': f_mod.model}
 
-        t1 = time.time()
-        for i in range(self.config['Niter']): #1000):
-            print(i)
-            a = np.einsum('gzst,gzt->gzs', Ap, v)
-            c = np.einsum('gzst,gzt->gzs', An, v)
 
-#            m0 = (-b + np.sqrt(b**2 + 4*a*c)) / (2*a)
-#            ipdb.set_trace()
+        t1 = time.time()
+        for i in range(self.config['Niter']):
+            a = np.einsum('gzst,gzt->gzs', Ap, v)
+
             m0 = b / a
             vn = m0*v
 
@@ -198,90 +195,19 @@ class bcnz_fit:
 
             v = vn
 
-#        ipdb.set_trace()
-
         print('time minimize',  time.time() - t1)
-
-        F = np.einsum('fsz,gzs->gfz', f_mod, v)
-        F = xr.DataArray(F, coords=coords, dims=('gal', 'f', 'z'))
+        F = np.einsum('zsf,gzs->gzf', f_mod, v)
+        F = xr.DataArray(F, coords=coords, dims=('gal', 'z', 'band'))
 
         chi2 = var_inv*(flux - F)**2
 
-        pb = np.exp(-0.5*chi2.sum(dim='f'))
+        pb = np.exp(-0.5*chi2.sum(dim='band'))
         pb = pb / (1e-100 + pb.sum(dim='z'))
-        chi2 = chi2.sum(dim='f')
+        chi2 = chi2.sum(dim='band')
 
         norm = xr.DataArray(v, coords=coords_norm, dims=('gal','z','model'))
 
-
         return chi2, norm
-
-    def chi2_marg(self, f_mod, data_df, zs): #, mabs_df):
-        print('starting the right class')
-
-#        data_df = data_df[100:150]
-        flux, flux_err, var_inv = self.get_arrays(data_df)
-
-        t1 = time.time()
-        K = np.einsum('gf,gf->g', flux**2, var_inv)
-#        B = np.einsum('gf,fsz->gzs', flux, f_mod)
-#        A = np.einsum('fsz,ftz->zst', f_mod, f_mod)
-
-        B = np.einsum('gf,fsz,gf->gzs', flux, f_mod, var_inv)
-        A = np.einsum('fsz,ftz,gf->gzst', f_mod, f_mod, var_inv)
-        print('time terms', time.time() - t1)
-
-#        ipdb.set_trace()
-
-        Ainv = np.zeros_like(A)
-        ngal, nz = A.shape[:2]
-
-        t1 = time.time()
-        # - Also loop over galaxies.
-        for i in range(ngal):
-            for j in range(nz):
-                Ainv[i,j] = np.linalg.inv(A[i,j])
-
-        print('time inverting', time.time() - t1)
-
-        # - Loop both over galaxies and redshifts...
-        Adet = np.zeros((ngal, nz))
-        for i in range(ngal):
-            for j in range(nz):
-                Adet[i,j] = np.linalg.det(A[i,j])
-
-#        pb = np.zeros(nz)
-        Fx = np.zeros((ngal, nz))
-        pb = np.zeros((ngal, nz))
-        for i in range(ngal):
-            for j in range(nz):
-                J = B[i,j]
-                pre = np.sqrt(2*np.pi/Adet[i,j])
-                F = np.dot(J, np.dot(Ainv[i,j], J))
-
-#                ipdb.set_trace()
-                print(F) #F-K[i]) 
-                pb[i,j] = pre*np.exp(0.5*(F-K[i]))
-                Fx[i,j] = F
-
-        # Well, some tests...
-        C = self.config
-        zgrid = np.arange(C['zmin'], C['zmax']+C['dz'], C['dz'])
-
-
-        for igal in range(len(Fx)):
-            P = np.exp(Fx[igal] - Fx[igal].min())
-            P = P / P.sum()
-
-            plt.plot(zgrid, P)
-
-            plt.axvline(zs.iloc[igal])
-
-            plt.show()
-
-        ipdb.set_trace()
-
-
 
     def odds_fast(self, pz, zb):
 
@@ -303,8 +229,8 @@ class bcnz_fit:
         z0 = z[0]
         bins1 = (z1 - z0) / dz - 1 # Cumsum is estimated at the end
         bins2 = (z2 - z0) / dz - 1
-        i1 = np.floor(bins1).astype(np.int)
-        i2 = np.floor(bins2).astype(np.int)
+        i1 = np.clip(np.floor(bins1), 0, np.infty).astype(np.int)
+        i2 = np.clip(np.floor(bins2), 0, np.infty).astype(np.int)
         db1 = bins1 - i1
         db2 = bins2 - i2
 
@@ -364,13 +290,14 @@ class bcnz_fit:
         f_algo = getattr(self, key)
 
 #        galcat = self.job.galcat.result
-        ab = self.job.ab.result.unstack()
+        ab = self.job.ab.result #.unstack()
         ab_el = self.job.ab_lines.result if hasattr(self.job, 'ab_lines') \
                 else None
         f_mod = self.model(ab, ab_el)
 
         galcat_store = self.job.galcat.get_store()
-        Rin = galcat_store.select('default', iterator=True, chunksize=10)
+        chunksize = 10
+        Rin = galcat_store.select('default', iterator=True, chunksize=chunksize)
 
         zs = False
 #        zs = self.job.zspec.result.zs
@@ -378,6 +305,8 @@ class bcnz_fit:
         path = self.job.empty_file('default')
         store = pd.HDFStore(path)
         for i,galcat in enumerate(Rin):
+            print('batch', i, 'tot', i*chunksize)
+
             chi2, norm = f_algo(f_mod, galcat, zs)
             peaks = self.photoz(chi2, norm)
 
