55e2250f16ef44b78f75106e3a14efd8acdd438c
Martin B. Eriksen <eriksen@pic.es>
2017-11-09 00:59:31 +0100

    being able to output best model

--- bcnz/tasks/bcnz_comb_ext.py
+++ bcnz/tasks/bcnz_comb_ext.py
@@ -14,7 +14,7 @@ sys.path.append('/home/eriksen/source/bcnz/bcnz/tasks')
 import libpzqual
 
 class bcnz_comb_ext:
-    version = 1.02
+    version = 1.07
     config = {'use_pz': False, 'flat_priors': True,
               'odds_lim': 0.01, 'width_frac': 0.01}
 
@@ -57,17 +57,44 @@ class bcnz_comb_ext:
         return pz
 
 
-    def combine_pz(self, pz):
+    def combine_pz(self, pz_in, cat_in):
         if self.config['flat_priors']:
-            priors = xr.DataArray(np.ones(len(pz.EBV)), dims=('EBV'))
+            priors = xr.DataArray(np.ones(len(pz_in.EBV)), dims=('EBV'))
         else:
-            raise NotImplementedError('BLAH...')
+            # COSMOS priors....
+            S = [(0.00, 31334), (0.10, 12927), (0.15, 12131), (0.20, 11778), (0.25, 11025), \
+                 (0.05, 10659), (0.30,  9055), (0.35,  6884), (0.40,  6067), (0.50,  5580)]
 
-        pz = pz.dot(priors)
+            E = pd.Series(dict(S), name='EBV').to_xarray()
+            priors = E.rename({'index': 'EBV'})
 
+            # Priors from the catalogue itself...
+            E = cat_in.loc[cat_in.groupby('gal').chi2.idxmin()].EBV.value_counts().to_xarray()
+            priors = E.rename({'index': 'EBV'})
+
+
+        priors = priors / float(priors.sum())
+
+        chi2_min = cat_in[['gal', 'EBV', 'chi2']].set_index(['gal', 'EBV']).to_xarray().chi2
+
+        pz = np.clip(pz_in, 1e-100, np.infty)
+        chi2_in = -2.*np.log(pz)
+
+        chi2_tmp = chi2_in.min(dim=['z'])
+        delta_chi2 = chi2_in + (chi2_min - chi2_tmp)
+
+        pz_ebv = np.exp(-0.5*delta_chi2)
+
+        pz = (pz_ebv*priors).sum(dim='EBV')
+
+        # Had 2 galaxies of 500 being Nan..
+        pz[np.isnan(pz).all(axis=1)] = 1.
         pz /= pz.sum(dim='z')
+        pz[np.isnan(pz).all(axis=1)] = 1./float(len(pz.z))
+
         izmin = pz.argmax(dim='z')
         zb = pz.z[izmin]
+
 
         pzcat = pd.DataFrame(index=pz.gal)
         pzcat['odds'] = libpzqual.odds(pz, zb, self.config['odds_lim'])
@@ -83,13 +110,51 @@ class bcnz_comb_ext:
 
         return cat_out
 
+    def load_models(self):
+        D = {}
+
+        for key,dep in self.job.depend.items():
+            print('loading', key)
+
+            # Since it also has to depend on the galaxy catalogs.
+            if not key.startswith('pzcat_'):
+                continue
+
+            best_model = dep.get_store()['best_model'] #.reset_index()
+
+            EBV = dep.f_mod.ab.config['EBV']
+            D[EBV] = best_model
+
+        return D
+
+    def get_best_model(self, cat_in, D): #best_models):
+        F = pd.concat(D, names=['EBV']).reset_index().set_index(['gal', 'EBV'])
+
+        tosel = cat_in.loc[cat_in.groupby('gal').chi2.idxmin()]
+        tosel = tosel.set_index(['gal', 'EBV'])
+
+        nbest = tosel[[]].join(F)
+
+        return nbest
+
     def run(self):
         if not self.config['use_pz']:
             cat_in = self.load_catalogs()
             pzcat = self.combine_cat(cat_in)
             pzcat = pzcat.set_index('gal')
         else:
+            cat_in = self.load_catalogs()
             pdf_in = self.load_pdf()
-            pzcat = self.combine_pz(pdf_in)
+            pzcat = self.combine_pz(pdf_in, cat_in)
 
+
+        # Estimate best model..
         self.job.result = pzcat.stack()
+        _models = self.load_models()
+        best_model = self.get_best_model(cat_in, _models)
+
+        path_out = self.job.empty_file('default')
+        store = pd.HDFStore(path_out, 'w')
+        store['pzcat'] = pzcat.stack()
+        store['best_model'] = best_model
+        store.close()
