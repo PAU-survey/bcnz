ddfe50f7683345c9b596f42a60680ee5ac2ff0f9
Martin B. Eriksen <eriksen@pic.es>
2017-11-23 23:35:05 +0100

    Weird bug.

--- bcnz/tasks/bcnz_select.py
+++ bcnz/tasks/bcnz_select.py
@@ -10,7 +10,7 @@ descr = {'use_nondet': 'If including non-detections',
 class bcnz_select:
     """Selecting a subset of the fluxes to use in the fitting."""
 
-    version = 1.04
+    version = 1.06
     config = {'SN_lim': -100., 'SN_cap': 10000}
 
     def limit_SN(self, cat):
@@ -19,35 +19,19 @@ class bcnz_select:
         SN = cat['flux'] / cat['flux_err']
 
         SN_lim = self.config['SN_lim']
-        cat[SN < SN_lim] = np.nan
+        cat['flux'] = cat['flux'][SN_lim < SN]
+
 
         SN_cap = self.config['SN_cap']
         new_SN = np.clip(SN, -np.infty, SN_cap)
-
         cat['flux_err'] = cat.flux / new_SN
-
-        return cat
-
-    def to_outformat(self, cat_in):
-        """Convert to hirarchical columns and perform some weird temporary scaling."""
-        
-        cat_in = cat_in.reset_index()
-
-        flux = cat_in.pivot('ref_id', 'band', 'flux')
-        flux_err = cat_in.pivot('ref_id', 'band', 'flux_err')
-
-        cat = pd.concat({'flux': flux, 'flux_err': flux_err}, axis=1)
 
         return cat
 
     def get_cat(self, cat):
         """Apply the different cuts."""
 
-        if 2 < len(cat.columns):
-            cat = cat[['flux', 'flux_err']].stack()
-
         cat = self.limit_SN(cat)
-        cat = self.to_outformat(cat)
 
         return cat
 
