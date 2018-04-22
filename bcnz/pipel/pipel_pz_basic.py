7e9580cf7083f572e589afa869506e62a47b2c71
Martin B. Eriksen <eriksen@pic.es>
2018-04-22 21:18:43 +0200

    Moving more pipelines here.

---
+++ bcnz/pipel/pipel_pz_basic.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# encoding: UTF8
+
+import copy
+import ipdb
+import numpy as np
+
+import xdolphin as xd
+import libtag
+import libcommon
+
+def get_ab():
+    """The input to the flux model."""
+
+    seds = xd.Job('curves')
+    seds.config.update({'input_dir': '/home/eriksen/data/photoz/seds/cosmos_self', 'suf': 'sed', 'min_val': 0.0})
+
+    extinction = xd.Job('extinction_lagaile')
+
+    ab_cont = xd.Job('ab_cont')
+    ab_cont.config.update({'dz_ab': 0.001, 'zmax_ab': 2.05})
+    ab_cont.depend['seds'] = seds
+    ab_cont.depend['filters'] = libcommon.Common('filters')
+    ab_cont.depend['extinction'] = extinction
+
+    ab_lines = xd.Job('emission_lines')
+    ab_lines.depend['filters'] = libcommon.Common('filters')
+    ab_lines.depend['extinction'] = extinction
+
+    return ab_cont, ab_lines
+
+def get_pzcat_config():
+    """The default photo-z configuration."""
+
+    config = {
+      'dz': 0.001,
+      'zmax': 1.0,
+      'Niter': 200,
+    #  'Niter': 400,
+    }
+
+    config['filters'] = list(map('NB{}'.format, range(455, 845+1, 10)))
+    # Add the SEDs..
+    ell = ['ell2','ell5','ell13']
+    sp = ['s0','sa','sb','sc','sd']
+    sb = sb = map('sb{}'.format, range(11))
+    seds = ell + sp + list(sb)
+    config['seds'] = seds
+
+    return config
+
+def get_model():
+    """The part returning the model."""
+
+    model = xd.Job('flux_model')
+    model.config.update({'dz': 0.001, 'zmax': 1.0})
+    ab, ab_lines = get_ab()
+
+    model.depend['ab'] = ab
+    model.depend['ab_lines'] = ab_lines
+
+    return model
+
+def get_galcat():
+    """Two steps for first selecting a subset of galaxies and
+       then one selection internally in the 
+    """
+
+    nbsubset = xd.Job('nbpzsubset')
+    nbsubset.depend['input'] = libcommon.Common('galcat')
+    nbsubset.depend['ref_cat'] = libcommon.Common('ref_cat')
+
+    select_data = xd.Job('bcnz_select')
+    select_data.depend['input'] = nbsubset
+    select_data.config['SN_lim'] = 2.6
+
+    return select_data
+
+def pipel():
+    """The main entry point for getting the pipeline."""
+
+    pzcat = xd.Job('bcnz_fit')
+    pzcat.config.update(get_pzcat_config())
+    pzcat.depend['model'] = get_model()
+    pzcat.depend['galcat'] = get_galcat()
+
+    pzcat.model.config['seds'] = pzcat.config['seds'] # yes...
+
+    return pzcat
---
+++ bcnz/pipel/pipel_pz_chunks.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# encoding: UTF8
+
+from IPython.core import debugger as ipdb
+import numpy as np
+#import pandas as pd
+
+import libcommon
+
+import pipel_photoz
+import pz_chunks
+
+import xdolphin as xd
+
+def get_model(part, model, bbsyn_coeff):
+    """Model for a specific chunk."""
+
+    # The shallow copy here ends up being very confusing...
+    xmodel = model.shallow_copy()
+    xab = model.ab.shallow_copy()
+    xab_lines = model.ab_lines.shallow_copy()
+    xmodel.config['sep_lines'] = part['sep_lines']
+
+    seds = xab.seds.shallow_copy()
+    seds.config['input_dir'] = part.sed_dir
+    xab.depend['seds'] = seds
+
+    xmodel.config['use_lines'] = part['use_lines']
+    conf = {'EBV': part['EBV'], 'ext_law': part['ext_law']}
+
+    xab.config.update(conf)
+    xab_lines.config.update(conf)
+    xmodel.config['seds'] = part['seds']
+
+    xmodel.depend['ab'] = xab 
+    xmodel.depend['ab_lines'] = xab_lines
+
+    ymodel = xd.Job('fmod_adjust')
+    ymodel.config['norm_band'] = 'subaru_r'
+    ymodel.depend['bbsyn_coeff'] = bbsyn_coeff
+    ymodel.depend['model'] = xmodel
+
+    return ymodel
+
+
+def pipel(chunks=False, prevot_calib=True, prevot_pzrun=False):
+    """Pipeline for BCNZv2 when running with many chunks."""
+
+    # chunks - Dataframe specifying the chunks to use. If not specified, use
+    #          default.
+    # prevot_calib - If including Prevot extinction for the calibration run.
+    # prevot_pzrun - If including Prevot extinction for the photo-z run.
+
+    pzcat_orig = pipel_photoz.pipel()
+
+    # Default bands.
+    X = 455 + 10*np.arange(40)
+    NB = list(map('NB{}'.format, X))
+    BB = ['cfht_u', 'subaru_B', 'subaru_V', 'subaru_r', 'subaru_i', 'subaru_z']
+
+    if not chunks:
+        chunks = pz_chunks.pz_chunks()
+
+    # Synthetic broad band coefficients used to scale the 
+    # broad band fluxes.
+    bbsyn_coeff = xd.Job('bbsyn_coeff')
+    bbsyn_coeff.depend['filters'] = libcommon.Common('filters') #pipel1.get('join_output')
+
+    # These are needed both for calibration and the photo-z.
+    modelD = {}
+    model = pzcat_orig.model
+    for key, row in chunks.iterrows():
+        modelD[key] = get_model(row, model, bbsyn_coeff)
+
+    # Calibration by comparing the model fit to the observations.
+    inter_calib = xd.Job('inter_calib')
+    inter_calib.depend['bbsyn_coeff'] = bbsyn_coeff
+    inter_calib.depend['galcat'] = libcommon.Common('galcat') #pipel.get('bcnz_select')
+
+    # These should be elsewhere...
+    inter_calib.config.update({\
+      'Nrounds': 15,
+      'learn_rate': 1.0,
+      'fit_bands': NB + BB,
+      'bb_norm': 'subaru_r',
+      'zp_min': 'flux2'})
+
+    for key, row in chunks.iterrows():
+        if (not prevot_calib) and (row.ext_law == 'SMC_prevot'):
+            continue
+
+        inter_calib.depend['model_{}'.format(key)] = modelD[key]
+
+    # Linking the photo-z runs.
+    bcnz_comb_ext = xd.Job('bcnz_comb_ext')
+    for key,row in chunks.iterrows():
+        if (not prevot_pzrun) and (row.ext_law == 'SMC_prevot'):
+            continue
+
+        pzcat = pzcat_orig.shallow_copy()
+        pzcat.depend['model'] = modelD[key]
+        pzcat.depend['galcat'] = libcommon.Common('galcat')
+        bcnz_comb_ext.depend['pzcat_{}'.format(key)] = pzcat
+
+    return bcnz_comb_ext
