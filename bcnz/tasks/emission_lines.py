c305315480a1fb8915780896e0ae2cd1c8896132
Martin B. Eriksen <eriksen@pic.es>
2018-04-29 01:54:41 +0200

    Separating OIII.

--- bcnz/tasks/emission_lines.py
+++ bcnz/tasks/emission_lines.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 
-from IPython.core import debugger
+from IPython.core import debugger as ipdb
 import os
 import time
 import numpy as np
@@ -16,7 +16,8 @@ descr = {
   'dz': 'Redshift steps',
   'ampl': 'Added amplitude',
   'EBV': 'Extinction coefficient',
-  'ext_law': 'Extinction law'}
+  'ext_law': 'Extinction law',
+  'sep_OIII': 'If keeping OIII separate'}
 
 class emission_lines:
     """The model flux for the emission lines."""
@@ -24,34 +25,7 @@ class emission_lines:
     version = 1.057
 
     config = {'dz': 0.0005, 'ampl': 1e-16, 'EBV': 0.,
-              'ext_law': 'SB_calzetti'}
-
-#    ratios = {
-#      'OII': 1.0,
-#      'OIII_1': 0.25*0.36,
-#      'OIII_2': 0.75*0.36,
-#      'Hbeta': 0.61,
-#      'Halpha': 1.77,
-#      'Lyalpha':  2.,
-#      'NII_1': 0.3 * 0.35 * 1.77, # Paper gave lines relative to Halpha.
-#      'NII_2': 0.35 * 1.77,
-#      'SII_1': 0.35,
-#      'SII_2': 0.35
-#    }
-
-    line_loc = {
-      'OII': 3726.8,
-      'OIII_1': 4959.,
-      'OIII_2': 5007.,
-      'Halpha': 6562.8,
-      'Hbeta': 4861,
-      'Lyalpha': 1215.7,
-      'NII_1': 6548., 
-      'NII_2': 6583.,
-      'SII_1': 6716.44,
-      'SII_2': 6730.82
-    }
-
+              'ext_law': 'SB_calzetti', 'sep_OIII': True}
 
     def _filter_spls(self, filters):
         """Convert filter curves to splines."""
@@ -61,7 +35,6 @@ class emission_lines:
         for fname in filters.index.unique():
             sub = filters.ix[fname]
             splD[fname] = splrep(sub.lmb, sub.response)
-
             rconstD[fname] = simps(sub.response/sub.lmb, sub.lmb)
 
         return splD, rconstD
@@ -82,19 +55,23 @@ class emission_lines:
         EBV = self.config['EBV']
         ampl = self.config['ampl']
 
-        flux = {}
-        for key, ratio in ratios.items():
-            lmb = self.line_loc[key]*(1+z)
+        # Note: This is not completely general.
+        fluxD = {'lines': 0., 'OIII': 0.}
+        for line_name, line in ratios.iterrows():
+            lmb = line.lmb*(1+z)
             y_f = splev(lmb, f_spl, ext=1) 
 
             k_ext = splev(lmb, ext_spl, ext=1) 
             y_ext = 10**(-0.4*EBV*k_ext)
 
-            flux[key] = (ampl*ratio*y_f*y_ext) / rconst
+            isOIII = line_name.startswith('OIII')
+            dest = 'OIII' if (isOIII and self.config['sep_OIII']) else 'lines'
+            fluxD[dest] += ampl*(line.ratio*y_f*y_ext) / rconst
 
-        flux['lines'] = sum(flux.values())
+        if not self.config['sep_OIII']:
+            del fluxD['OIII']
 
-        return flux
+        return fluxD
 
     def _new_fix(self, oldD, z, band, ext_law, EBV):
         """Dictionary suitable for concatination."""
@@ -137,11 +114,25 @@ class emission_lines:
 
         return df
 
+    def line_model(self, ratios, filters, extinction):
+        """Find the emission line model."""
 
-    def entry(self, ratios, filters, extinction):
         filtersD, rconstD = self._filter_spls(filters)
         ext_spl = self.ext_spl(extinction)
-        model = self.get_model(ratios, filtersD, rconstD, ext_spl)
+
+        z = np.arange(0., 2., self.config['dz'])
+
+#        df = pd.DataFrame()
+        for fname, f_spl in filtersD.items():
+            rconst = rconstD[fname]     
+            part = self._find_flux(z, f_spl, ratios, rconst, ext_spl)
+
+            ipdb.set_trace()
+
+
+
+    def entry(self, ratios, filters, extinction):
+        model = self.line_model(ratios, filters, extinction) #filtersD, rconstD, ext_spl)
 
         return model
 
