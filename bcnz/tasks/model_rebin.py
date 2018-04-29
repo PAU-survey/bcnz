137541aa2bbbdfe3261d826a758d0dea225f2b03
Martin B. Eriksen <eriksen@pic.es>
2018-04-29 15:58:04 +0200

    Code for rebinning in redshift.

---
+++ bcnz/tasks/model_rebin.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# encoding: UTF8
+
+from IPython.core import debugger as ipdb
+import time
+import numpy as np
+import pandas as pd
+from scipy.interpolate import splrep, splev, splint
+
+descr = {
+  'zmin': 'Minimum redshift',
+  'zmax': 'Maximum redshift',
+  'dz': 'Grid width in redshift'
+}
+
+class model_rebin:
+    """Rebing the redshift grid of the model."""
+
+    version = 1.0
+    config = {'zmin': 0.01, 'zmax': 1.2, 'dz': 0.001}
+  
+    # This rebinning is probably not needed. Instead on can estimate
+    # the model in the correct binning in the first place. I need this
+    # by now to compare the pipelines. 
+
+    def entry(self, model):
+        C = self.config
+        zgrid = np.arange(C['zmin'], C['zmax']+C['dz'], C['dz'])
+
+        inds = ['band', 'sed', 'ext_law', 'EBV']
+        model = model.reset_index().set_index(inds)
+
+        print('starting to rebin')
+        t1 = time.time()
+        rebinned = pd.DataFrame()
+        for key in model.index.unique():
+            sub = model.loc[key]
+            spl = splrep(sub.z, sub.flux)
+
+            part = pd.DataFrame({'z': sub.z, 'flux': splev(sub.z, spl)})
+            rebinned = rebinned.append(part)
+
+        print('time', time.time() - t1)
+        rebinned = rebinned.reset_index().set_index(inds)
+
+        return rebinned
+
+    def run(self):
+        model = self.input.model.result
+        self.output.result = self.entry(model)
+
