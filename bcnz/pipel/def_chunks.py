811dd62da3d465a43b16838dbaaced4b9944207c
Martin B. Eriksen <eriksen@pic.es>
2018-04-30 14:37:51 +0200

    Removing SB2 and SB3 from the SB extincion list.

--- bcnz/pipel/def_chunks.py
+++ bcnz/pipel/def_chunks.py
@@ -30,7 +30,7 @@ def pz_chunks():
     # 1 - If including extinction lines.
     # 2 - The extinction law.
     # 3 - SEDs used.
-    sb = ['SB2_A_0','SB3_A_0','SB4_A_0','SB5_A_0','SB6_A_0','SB7_A_0','SB8_A_0','SB9_A_0','SB10_A_0','SB11_A_0']
+    sb = ['SB4_A_0','SB5_A_0','SB6_A_0','SB7_A_0','SB8_A_0','SB9_A_0','SB10_A_0','SB11_A_0']
     mix = ['Sc_A_0', 'Sd_A_0', 'Sdm_A_0', 'SB0_A_0', 'SB1_A_0', 'SB2_A_0', 'SB3_A_0', 'SB4_A_0']
     with_ext = [(True, 'SB_calzetti', sb),
                 (True, 'SB_calzetti_bump1', sb),
@@ -54,6 +54,5 @@ def pz_chunks():
             S['seds'] = seds
 
             df = df.append(S, ignore_index=True)
-
 
     return df
