6837fdb48fb1649f7a373e1724f8d966f3c68d6e
Martin B. Eriksen <eriksen@pic.es>
2018-04-30 13:42:21 +0200

    Update chunks

--- bcnz/pipel/def_chunks.py
+++ bcnz/pipel/def_chunks.py
@@ -14,73 +14,46 @@ def pz_chunks():
 
     sed_dir = '~/data/photoz/seds/cosmos_noext'
 
+
+    # The configurations without extinction.
+    # 1 - If including extinction lines.
+    # 2 - SEDs used.
+    noext = [(False, ['Ell1_A_0', 'Ell2_A_0', 'Ell3_A_0', 'Ell4_A_0', 'Ell5_A_0','Ell6_A_0']),
+             (False, ['Ell6_A_0', 'Ell7_A_0', 'S0_A_0','Sa_A_0','Sb_A_0', 'Sc_A_0']),
+             (True, ['Sc_A_0', 'Sd_A_0', 'Sdm_A_0', 'SB0_A_0','SB1_A_0','SB2_A_0']),
+             (True, ['SB2_A_0','SB3_A_0','SB4_A_0','SB5_A_0','SB6_A_0','SB7_A_0','SB8_A_0','SB9_A_0','SB10_A_0','SB11_A_0']),
+             (False, ['bc2003_lr_m52_chab_tau03_dust00_age0.509', 'bc2003_lr_m52_chab_tau03_dust00_age8.0', \
+                      'bc2003_lr_m62_chab_tau03_dust00_age0.509', 'bc2003_lr_m62_chab_tau03_dust00_age2.1', \
+                      'bc2003_lr_m62_chab_tau03_dust00_age2.6', 'bc2003_lr_m62_chab_tau03_dust00_age3.75'])]
+
+    # The configurations with the extinction.
+    # 1 - If including extinction lines.
+    # 2 - The extinction law.
+    # 3 - SEDs used.
+    sb = ['SB2_A_0','SB3_A_0','SB4_A_0','SB5_A_0','SB6_A_0','SB7_A_0','SB8_A_0','SB9_A_0','SB10_A_0','SB11_A_0']
+    mix = ['Sc_A_0', 'Sd_A_0', 'Sdm_A_0', 'SB0_A_0', 'SB1_A_0', 'SB2_A_0', 'SB3_A_0', 'SB4_A_0']
+    with_ext = [(True, 'SB_calzetti', sb),
+                (True, 'SB_calzetti_bump1', sb),
+                (True, 'SB_calzetti_bump2', sb),
+                (True, 'SMC_prevot', mix)]
+
     df = pd.DataFrame()
-    # Data structure to define how the different chunks should be run. Use:
-    # 1 - If including emission lines.
-    # 2 - Extinction laws.
-    # 3 - The SEDs.
-
-    calzetti = ['SB_calzetti', 'SB_calzetti_bump1', 'SB_calzetti_bump2']
-    X = [(False, calzetti, ['Ell1_A_0', 'Ell2_A_0', 'Ell3_A_0', 'Ell4_A_0', 'Ell5_A_0','Ell6_A_0']),
-         (False, calzetti, ['Ell6_A_0', 'Ell7_A_0', 'S0_A_0','Sa_A_0','Sb_A_0', 'Sc_A_0']),
-         (True, prevot ['Sc_A_0', 'Sd_A_0', 'Sdm_A_0', 'SB0_A_0','SB1_A_0','SB2_A_0']),
-         (True, True, ['SB2_A_0','SB3_A_0','SB4_A_0','SB5_A_0','SB6_A_0','SB7_A_0','SB8_A_0','SB9_A_0','SB10_A_0','SB11_A_0']),
-         (False, True, ['bc2003_lr_m52_chab_tau03_dust00_age0.509', 'bc2003_lr_m52_chab_tau03_dust00_age8.0', \
-                        'bc2003_lr_m62_chab_tau03_dust00_age0.509', 'bc2003_lr_m62_chab_tau03_dust00_age2.1', \
-                        'bc2003_lr_m62_chab_tau03_dust00_age2.6', 'bc2003_lr_m62_chab_tau03_dust00_age3.75'])]
-
-    X = pd.DataFrame(X, columns=['use_line', 'use_ext', 'seds'])
-
-    # The values to use for the extinction.
-    laws = ['SB_calzetti', 'SB_calzetti_bump1', 'SB_calzetti_bump2', 'SMC_prevot']
-    EBV_values = np.arange(0.05, 0.501, 0.05)
-
-    # And then expand this into the actual configuration..
-    df = pd.DataFrame()
-    for key, row in X.iterrows():
-        S = row.copy()
-        if not row.use_ext:
-            S['EBV'] = 0.
-            S['ext_law'] = 'SB_calzetti'
-            df = df.append(S, ignore_index=True)
-
-            continue
-
-        for ext_law in laws:
-            for EBV in EBV_values:
-                S = row.copy()
-                S['ext_law'] = ext_law
-                S['EBV'] = EBV
-
-                df = df.append(S, ignore_index=True)
-
-
-    ipdb.set_trace()
-
-
-
-    X = [(True, other_seds)]
-
-    # Configurations without without extinction
-    for use_lines, seds in X:
-        S = pd.Series({'seds': seds, 'EBV': 0., 'ext_law': 'SB_calzetti',
-                       'use_lines': use_lines})
-        S['sed_dir'] = sed_dir
+    # Here the no-extinction runs use the Calzetti law with EBV=0.
+    for use_lines, seds in noext:
+        S = pd.Series({'ext_law': 'SB_calzetti', 'EBV': 0.})
+        S['use_lines'] = use_lines
+        S['seds'] = seds
 
         df = df.append(S, ignore_index=True)
 
-    # The star burst configurations...
-    with_ext = ['SB4_A_0', 'SB5_A_0','SB6_A_0','SB7_A_0','SB8_A_0','SB9_A_0','SB10_A_0','SB11_A_0']
-    laws = ['SB_calzetti', 'SB_calzetti_bump1', 'SB_calzetti_bump2', 'SMC_prevot']
     EBV_values = np.arange(0.05, 0.501, 0.05)
-
-    for ext_law in laws:
+    for use_lines, ext_law, seds in with_ext:
         for EBV in EBV_values:
-            S = pd.Series({'seds': with_ext, 'EBV': EBV, 'ext_law': ext_law,
-                           'use_lines': True})
-            S['sed_dir'] = sed_dir
+            S = pd.Series({'ext_law': ext_law, 'EBV': EBV})
+            S['use_lines'] = use_lines
+            S['seds'] = seds
+
             df = df.append(S, ignore_index=True)
 
-    df['sep_OIII'] = True
 
     return df
