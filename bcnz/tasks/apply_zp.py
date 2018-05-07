307e51c070d173c041c6dfb5367f8ccf715400bf
Martin B. Eriksen <eriksen@pic.es>
2018-05-07 16:10:26 +0200

    Applying zero-points

---
+++ bcnz/tasks/apply_zp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: UTF8
+
+from IPython.core import debugger as ipdb
+
+descr = {}
+
+class apply_zp:
+    """Apply zero-points per band."""
+
+    version = 1.0
+    config = {}
+
+    # Note: One can consider adding the functionality
+    # of specifying some zero-points by the configuration.
+    def entry(self, galcat, zp):
+        # Applying this inline is simpler.
+        for band, zp_val in zp.items():
+            galcat[('flux', band)] *= zp_val
+            galcat[('flux_err', band)] *= zp_val
+
+        return galcat
+
+    def run(self):
+        galcat = self.input.galcat.result
+        zp = self.input.zp.result
+
+        self.output.result = self.entry(galcat, zp)
