ebf878b680338d7768e2f61f61ef5b2b2ae3aee1
Martin B. Eriksen <eriksen@pic.es>
2019-05-15 16:38:58 +0200

    Functionality for loading modules

--- bcnz/lib/utils.py
+++ bcnz/lib/utils.py
@@ -2,11 +2,28 @@
 # encoding: UTF8
 
 import pandas as pd
+from tqdm import tqdm
 
 def flatten(df):
+    """Flattens the input dataframe."""
+
     flux = df.flux.rename(columns=lambda x: f'flux_{x}')
     flux_err = df.flux_err.rename(columns=lambda x: f'flux_err_{x}')
 
     comb = pd.concat([flux, flux_err], 1)
     
     return comb
+
+def load_models(job, bands):
+    """Load the models corresponding to a photo-z job."""
+    
+    modelD = {}
+    for key,dep in tqdm(job.depend.items()):
+        part = dep.model.result.to_xarray().flux.squeeze()
+        part = part.sel(band=bands).transpose('z', 'band', 'sed')
+        part = part.rename(sed='model')
+        nr = int(key.replace('pzcat_', ''))
+        
+        modelD[nr] = part
+        
+    return modelD
