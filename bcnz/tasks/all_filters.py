52fb27ba6b2426971e53fed4c95d779eec5a7e2a
Martin B. Eriksen <eriksen@pic.es>
2019-07-13 19:19:03 +0200

    local changes

---
+++ bcnz/tasks/all_filters.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: UTF8
+
+from pathlib import Path
+import pandas as pd
+from glob import glob
+
+class all_filters:
+    config = {}
+    version = 1.01
+
+    def entry(self):
+
+        dfilters = '/home/eriksen/data/photoz/all_filters/v2'
+
+        L = []
+        for path in glob(str(Path(dfilters) / '*')):
+            part = pd.read_csv(path, names=['lmb', 'response'])
+            part['band'] = Path(path).with_suffix('').name
+            
+            L.append(part)
+
+        df = pd.concat(L, ignore_index=True)
+        df = df.set_index('band')
+
+        return df
+
+    def run(self):
+        result = self.entry()
+        self.output.result = result
