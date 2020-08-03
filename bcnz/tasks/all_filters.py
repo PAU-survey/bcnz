22609f95e4f03f72083db8bcfbd8536d64a1d65b
Martin B. Eriksen <eriksen@pic.es>
2020-08-03 16:41:12 +0200

    update

--- bcnz/tasks/all_filters.py
+++ bcnz/tasks/all_filters.py
@@ -8,18 +8,22 @@ from glob import glob
 
 class all_filters:
     config = {}
-    version = 1.01
+    version = 1.11
 
     def entry(self):
 
-        dfilters = '~/data/photoz/all_filters/v2'
+        # Upgraded directory to include the CFHT filters.
+        dfilters = '~/data/photoz/all_filters/v3'
         dfilters = os.path.expanduser(dfilters)
 
         L = []
-        for path in glob(str(Path(dfilters) / '*')):
-            part = pd.read_csv(path, names=['lmb', 'response'])
-            part['band'] = Path(path).with_suffix('').name
+        for x in glob(str(Path(dfilters) / '*')):
+            path = Path(x)
             
+            sep = ',' if path.suffix == '.csv' else ' '
+            part = pd.read_csv(x, names=['lmb', 'response'], comment='#', sep=sep)
+            part['band'] = path.with_suffix('').name
+
             L.append(part)
 
         assert len(L), 'Filters not found.'
@@ -27,6 +31,7 @@ class all_filters:
         df = df.set_index('band')
 
         return df
+
 
     def run(self):
         result = self.entry()
