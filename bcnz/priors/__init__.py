0e112bcd671822e2b75d87c932c10fba739e640f
Martin B. Eriksen <eriksen@pic.es>
2020-09-04 15:47:10 +0200

    removing and moving around

--- bcnz/priors/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# encoding: UTF8
-__all__ = ['pau', 'fit_train']
-
-from bcnz.priors.pau import pau
-from bcnz.priors.train import train
-#from bcnz.priors.tess import tess
-from bcnz.priors.kde import kde
-from bcnz.priors.ext import ext
