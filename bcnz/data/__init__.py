2d940c46047ab51e61f91b6e66722dd90cbe15c6
Martin B. Eriksen <eriksen@pic.es>
2020-09-04 17:39:07 +0200

    Matching functionality

--- bcnz/data/__init__.py
+++ bcnz/data/__init__.py
@@ -2,3 +2,5 @@ from .cosmos_laigle import cosmos_laigle
 from .paudm_coadd import paudm_coadd
 
 from .connect_db import connect_db
+
+from .match_parent_cat import match_parent_cat
---
+++ bcnz/data/match_parent_cat.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: UTF8
+
+from IPython.core import debugger as ipdb
+import numpy as np
+import pandas as pd
+
+from sklearn.neighbors import KDTree
+
+def match_parent_cat(parent_cat, match_to, max_dist=0.9, drop_duplicates=True):
+    """Code for creating the match. Returns a new catalog with the
+       matched entries and a new index.
+    """
+
+    inst = KDTree(parent_cat[['ra', 'dec']])
+    dist, ind = inst.query(match_to[['ra', 'dec']])
+
+    index_name = parent_cat.index.name
+    mapping = pd.DataFrame({'dist': dist[:,0], \
+                            index_name: parent_cat.index[ind[:,0]], \
+                            'match_id': match_to.index})
+    mapping = mapping.set_index(index_name)
+
+    # Max separation in degrees.
+    max_dist = self.config['max_dist'] / 3600.
+    mapping = mapping[mapping.dist < max_dist]
+
+    # A bit convoluted, but needed since the merge is giving problems
+    # with hirarchical indexes.
+    match_to = match_to.loc[mapping.match_id]
+    match_to.loc[mapping.match_id, 'ref_id'] = mapping.index
+    match_to = match_to.set_index('ref_id')
+
+    # Double matches (found one..). Drop both.
+    if self.config['drop_duplicates']:
+        match_to = match_to.loc[match_to.index.drop_duplicates(False)]
+
+    return match_to
