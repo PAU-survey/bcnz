a426f7d8f5539cb8a7643ecfab770e0b2fca2834
Martin B. Eriksen <eriksen@pic.es>
2020-09-25 14:50:28 +0200

    Add the lisence.

--- bcnz/calib/cache.py
+++ bcnz/calib/cache.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2020 Martin B. Eriksen
+# This file is part of BCNz <https://github.com/PAU-survey/bcnz>.
+#
+# BCNz is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# BCNz is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with BCNz.  If not, see <http://www.gnu.org/licenses/>.
 # encoding: UTF8
 
 # Simple wrapper to cache the output.
